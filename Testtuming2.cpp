d89b8cdf2752b3d40764e00754ff7b3c272dbb13
Elsa Dwi Agresty <112572323+Elsadwiagresty07@users.noreply.github.com>
2022-09-22 00:38:21 +0700

    Add files via upload

---
+++ Testtuming2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+	string L;
+	cout << "Nama : ";
+	getline (cin,L);
+	cout << endl;
+
+
+	string P;
+	cout << "NPM : ";
+	getline (cin,P);
+	cout << endl;
+
+
+	string E;
+	cout << "Kelas : ";
+	getline (cin,E);
+	cout << endl;
+
+
+	string U;
+	cout << "Prodi : ";
+	getline (cin,U);
+	cout << endl;
+
+
+
+	cout << "Nama  :" << E <<endl;
+	cout << "NPM   :" << L <<endl;
+	cout << "Kelas :" << S <<endl;
+	cout << "Prodi :" << A <<endl;
+
+	return 0;
+
+
+}
